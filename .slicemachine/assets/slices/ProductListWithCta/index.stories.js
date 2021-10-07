9e08e0c2078477b57c36f89cdec93acc5831ab97
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2021-10-07 17:03:10 +0200

    adding variations for integration fields

--- .slicemachine/assets/slices/ProductListWithCta/index.stories.js
+++ .slicemachine/assets/slices/ProductListWithCta/index.stories.js
@@ -5,5 +5,8 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"product_list_with_cta","items":[{"productLink":{"link_type":"Web","url":"http://google.com"}},{"productLink":{"link_type":"Web","url":"https://slicemachine.dev"}}],"primary":{"title":[{"type":"heading1","text":"Whiteboard leading-edge initiatives","spans":[]}],"description":[{"type":"paragraph","text":"Cillum mollit ea minim. Officia et adipisicing do nulla esse ullamco enim pariatur.","spans":[]}]},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"product_list_with_cta","items":[{"productLink":{"link_type":"Web","url":"http://twitter.com"}},{"productLink":{"link_type":"Web","url":"http://google.com"}},{"productLink":{"link_type":"Web","url":"https://slicemachine.dev"}}],"primary":{"title":[{"type":"heading1","text":"Harness intuitive supply-chains","spans":[]}],"description":[{"type":"paragraph","text":"Id nulla ullamco eiusmod et et.","spans":[]}]},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
+
+export const _CustomProducts = () => <MyComponent slice={{"variation":"customProducts","name":"Custom Products","slice_type":"product_list_with_cta","items":[{"productLink":{"link_type":"Web","url":"http://google.com"}},{"productLink":{"link_type":"Web","url":"http://google.com"}},{"productLink":{"link_type":"Web","url":"http://twitter.com"}},{"productLink":{"link_type":"Web","url":"http://twitter.com"}},{"productLink":{"link_type":"Web","url":"http://twitter.com"}}],"primary":{"title":[{"type":"heading1","text":"Repurpose frictionless content","spans":[]}],"description":[{"type":"paragraph","text":"Aliquip reprehenderit aute adipisicing velit mollit incididunt et excepteur exercitation ea aute.","spans":[]}]},"id":"_CustomProducts"}} />
+_CustomProducts.storyName = 'Custom Products'
