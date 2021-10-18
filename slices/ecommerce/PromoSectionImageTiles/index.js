95e7acc4a75de59cebf1cd3a13ac41d7071e02fd
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2021-10-18 14:35:58 +0200

    adding background color

--- slices/ecommerce/PromoSectionImageTiles/index.js
+++ slices/ecommerce/PromoSectionImageTiles/index.js
@@ -5,7 +5,7 @@ import {linkResolver} from '../../../prismic-configuration'
 
 const PromoSectionImageTiles = ({ slice }) => (
   <section>
-    <div className="relative bg-white overflow-hidden">
+    <div className="relative overflow-hidden" style={{ backgroundColor: slice.primary.backgroundColor}}>
       <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
         <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static">
           <div className="sm:max-w-lg">
