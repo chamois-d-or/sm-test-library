2a6271e15beb47eb7c3fefe07c3446004f96d473
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2021-10-25 15:59:48 +0200

    adding label in textcontent

--- .slicemachine/assets/slices/marketing/ContentSectionCentered/index.stories.js
+++ .slicemachine/assets/slices/marketing/ContentSectionCentered/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"content_section_centered","items":[],"primary":{"description":[{"type":"paragraph","text":"Mollit anim amet amet pariatur deserunt duis veniam aliqua. Occaecat laboris ipsum eu occaecat nulla enim qui est sint laborum anim.","spans":[]}]},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"content_section_centered","items":[],"primary":{"description":[{"type":"paragraph","text":"Minim incididunt dolore magna non officia laboris veniam ut eiusmod exercitation aute enim. Duis reprehenderit anim do laboris irure eiusmod mollit cillum occaecat veniam enim in occaecat nisi commodo.","spans":[]}]},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
--- .slicemachine/assets/slices/marketing/ContentSectionCenteredImage/index.stories.js
+++ .slicemachine/assets/slices/marketing/ContentSectionCenteredImage/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"content_section_centered_image","items":[],"primary":{"description":[{"type":"paragraph","text":"Lorem ea excepteur nisi do occaecat ut sunt culpa est. Duis nisi enim duis voluptate id adipisicing. Nisi ea Lorem ad nostrud aute.","spans":[]}],"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1593642633279-1796119d5482?w=900&h=500&fit=crop"}},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"content_section_centered_image","items":[],"primary":{"description":[{"type":"paragraph","text":"Eiusmod nostrud aliqua elit exercitation nisi et veniam aliquip consequat elit ullamco quis velit adipisicing.","spans":[]}],"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1587613865763-4b8b0d19e8ab?w=900&h=500&fit=crop"}},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
