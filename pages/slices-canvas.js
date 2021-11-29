559679b58b8f3efd5a357da377c89fdc2ab2c697
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2021-11-29 18:18:40 +0100

    moving slicecanvas page

--- pages/slices-canvas.js
+++ pages/slices-canvas.js
@@ -1,8 +1,8 @@
 import { SliceCanvasRenderer } from "@prismicio/slice-canvas-renderer-react";
 import SliceZone from "next-slicezone";
 
-import state from "../../.slicemachine/libraries-state.json";
-import resolver from  "../../sm-resolver";
+import state from "../.slicemachine/libraries-state.json";
+import resolver from  "../sm-resolver";
 
 const SliceCanvas = () => (<SliceCanvasRenderer
 	// The `sliceZone` prop should be a function receiving slices and rendering them using your `SliceZone` component.
